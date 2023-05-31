6b2701494428211ee0fbf5cd7b57b64eb9ad974d
Aakash <aakashsuresh62@gmail.com>
2023-05-31 12:52:13 +0530

    routes added

--- src/App.js
+++ src/App.js
@@ -3,11 +3,22 @@ import Footer from "./components/Footer/Footer";
 import Hero from "./components/Hero/Hero";
 import Navbar from "./components/Navbar/Navbar";
 import Home from "./pages/Home";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import MyWorks from "./pages/MyWorks";
+import Products from "./pages/Products";
+import About from "./pages/About";
 
 function App() {
   return (
     <div className="App">
-      <Home />
+      <Router>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/myworks" element={<MyWorks />} />
+          <Route path="/products" element={<Products />} />
+          <Route path="/about" element={<About />} />
+        </Routes>
+      </Router>
     </div>
   );
 }
---
+++ src/pages/MyWorks.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function MyWorks() {
+  return (
+    <div className='works'>
+        <h1>My Works</h1>
+    </div>
+  )
+}
+
+export default MyWorks
