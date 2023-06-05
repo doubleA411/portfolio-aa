b5f4d4a1f1860686c74a83573870b7e0b43d763f
Aakash <aakashsuresh62@gmail.com>
2023-06-05 19:04:10 +0530

    works added

---
+++ src/data.js
@@ -0,0 +1,32 @@
+const works = [
+  {
+    title: "ARQe",
+    description:
+      "An Augemneted Reality based Qualtity education app for students.",
+    link: "https://github.com/doubleA411/arqe",
+  },
+
+  {
+    title: "Resume.io",
+    description: "Resume creating web application for university students.",
+    link: "https://github.com/doubleA411/resume.io",
+  },
+  {
+    title: "Gym workout tracker",
+    description: "Workout tracker for weight lifting drill using AI",
+    link: "https://github.com/doubleA411/gym-rep",
+  },
+  {
+    title: "Question Answering Bot in web application",
+    description: "Document based QnA bot hosted in web using flask and ngrok",
+    link: "https://github.com/doubleA411/Question-Answering-Model-on-Web",
+  },
+  {
+    title: "Twitter Clone",
+    description: "Built a twitter clone using ReactJS and Firebase",
+    link: "https://github.com/doubleA411/twitter-clone",
+  },
+];
+
+
+export default works;
--- src/pages/MyWorks/MyWorks.jsx
+++ src/pages/MyWorks/MyWorks.jsx
@@ -2,9 +2,14 @@ import React from 'react'
 import Navbar from '../../components/Navbar/Navbar'
 import GitHubCalendar from "react-github-calendar";
 import './MyWorks.css'
+import works from '../../data';
+import { ArrowRightIcon } from '@radix-ui/react-icons';
+import { Link } from 'react-router-dom';
+
+
 function MyWorks() {
   return (
-    <div className="works">
+    <div className="mywork">
       <Navbar />
       <div className="main">
         <h1>My Works</h1>
@@ -17,6 +22,25 @@ function MyWorks() {
           hideTotalCount
           hideColorLegend
         />
+        <div className="works">
+          <p className='sub'> Some best works</p>
+          {works.map((e, i) => {
+            return (
+              <div className="work">
+                <p className='title'>
+                  {e.title}{" "}
+                  <Link className='work-link' to={e.link}>
+                    <ArrowRightIcon width={20} height={20}/>
+                  </Link>
+                </p>
+                <p className='desc'>{e.description}</p>
+                <hr />
+              </div>
+            );
+          })}
+          <p className='allworks
+          '>To see all of my works <Link><ArrowRightIcon/></Link></p>
+        </div>
       </div>
     </div>
   );
