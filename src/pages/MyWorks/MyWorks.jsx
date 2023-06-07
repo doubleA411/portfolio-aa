367038eb8bb583156524177cb967eb92d0b0f547
Aakash <56353087+xii-aakash@users.noreply.github.com>
2023-06-07 21:40:53 +0530

    works page ui updated -2

--- src/pages/MyWorks/MyWorks.jsx
+++ src/pages/MyWorks/MyWorks.jsx
@@ -10,41 +10,43 @@ function MyWorks() {
   return (
     <div className="mywork">
       <Navbar />
-      <div className="main">
-        <p className="page-name">My Works</p>
+      <div className="flex-box">
+        <div className="main">
+          <p className="page-name">My Works</p>
 
-        <GitHubCalendar
-          username="doubleA411"
-          year={"last"}
-          colorScheme="light"
-          hideMonthLabels
-          hideTotalCount
-          hideColorLegend
-        />
-        <div className="works">
-          <p className="sub"> Some best works</p>
-          {works.map((e, i) => {
-            return (
-              <div className="work">
-                <Link className="work-link" to={e.link}>
-                  <p className="title">
-                    {e.title} <ArrowRightIcon width={20} height={20} />
-                  </p>
-                </Link>
-                <p className="desc">{e.description}</p>
-                <hr />
-              </div>
-            );
-          })}
-          <p
-            className="allworks
-          "
-          >
-            To see all of my works{" "}
-            <Link>
-              <ArrowRightIcon />
-            </Link>
-          </p>
+          <GitHubCalendar
+            username="doubleA411"
+            year={"last"}
+            colorScheme="light"
+            hideMonthLabels
+            hideTotalCount
+            hideColorLegend
+          />
+          <div className="works">
+            <p className="sub"> Some best works</p>
+            {works.map((e, i) => {
+              return (
+                <div className="work">
+                  <Link className="work-link" to={e.link}>
+                    <p className="title">
+                      {e.title} <ArrowRightIcon width={20} height={20} />
+                    </p>
+                  </Link>
+                  <p className="desc">{e.description}</p>
+                  <hr />
+                </div>
+              );
+            })}
+            <p className="allworks">
+              To see all of my works{" "}
+              <Link>
+                <ArrowRightIcon />
+              </Link>
+            </p>
+          </div>
+        </div>
+        <div className="hover-comp">
+          <p>Drag and drop work to display apllication / hover on works to display application</p>
         </div>
       </div>
     </div>
